67468acd0deef013d0bb1451066598cabdaad151
Champ-Dog <champdog.code@gmail.com>
2022-08-08 14:14:23 +1000

    Configured login

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/userModel");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 router.post("/register", async (req, res) => {
   try {
@@ -30,14 +31,60 @@ router.post("/register", async (req, res) => {
       .status(200)
       .send({ message: "User created successfully", success: true });
   } catch (error) {
-    // Throw + confirm error if user creation fails
-    res.status(500).send({ message: "Error creating user", success: false, error });
+    // Throw + confirm error if user creation fails. Log error in console for debugging
+    console.log(error);
+    res
+      .status(500)
+      .send({ message: "Error creating user", success: false, error });
   }
 });
 
 router.post("/login", async (req, res) => {
   try {
-  } catch (error) {}
+    // Search for user by provided email
+    const user = await User.findOne({ email: req.body.email });
+
+    // If no matching user found, don't execute the rest of the login logic, and return error message
+    if (!user) {
+      return res
+        .status(200)
+        .send({ message: "User does not exist", success: false });
+    }
+
+    // Check provided password against stored (encrypted) password
+    // Bcrypt 'compare' method handles this, so there is no need to explicitly handle encrypted password vs unencrypted password provided by user
+    const isMatch = await bcrypt.compare(req.body.password, user.password);
+
+    // If password doesn't match, don't execute further login logic, return error message
+    if (!isMatch) {
+      return res
+        .status(200)
+        .send({ message: "Password incorrect", success: false });
+    }
+
+    // Otherwise, proceed with login and return authentication (access) token
+    // JWT 'sign' method will generate the token. The first parameter for this method is the data JWT is to encrypt in the token payload
+    // The second parameter is the secret key to decode the token payload
+    // The third parameter provided is validity period for the token (how long until it expires). There is no default value for this, so one must
+    // be provided at some point in the process if we wish to restrict this
+    else {
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "2h",
+      });
+
+      // If login successful, confirm login and return token
+      res
+        .status(200)
+        // The returned data is only the token, so there is no need for an object (i.e., 'data: { token }').
+        .send({ message: "Login successful", success: true, data: token });
+    }
+  } catch (error) {
+    // On other errors (network issues, browser issues, etc.), return error message and details, and log error details in console for debugging
+    console.log(error);
+    res
+      .status(500)
+      .send({ message: "Error logging in", success: false, error });
+  }
 });
 
 module.exports = router;
